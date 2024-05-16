587ff2d8f692fc5c5a7085888c6ac781f6009f57
PhKhang <trannguyenphuckhang12@gmail.com>
2024-05-16 11:07:17 +0700

    Remove: android directory clean up

---
+++ android/app/build/intermediates/assets/debug/public/_nuxt/C7vxnhek.js
@@ -0,0 +1,3 @@
+import{U as i,V as a,W as c}from"./CTak-ZH6.js";import{f as d,s as l}from"./IHLlDrzm.js";/*!
+ * (C) Ionic http://ionicframework.com - MIT License
+ */const w=()=>{const e=window;e.addEventListener("statusTap",()=>{i(()=>{const n=e.innerWidth,s=e.innerHeight,o=document.elementFromPoint(n/2,s/2);if(!o)return;const t=d(o);t&&new Promise(r=>a(t,r)).then(()=>{c(async()=>{t.style.setProperty("--overflow","hidden"),await l(t,300),t.style.removeProperty("--overflow")})})})})};export{w as startStatusTap};
