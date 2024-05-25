eaa0e5d057a15f909e22711c014a8c6ef9e9907f
PhKhang <trannguyenphuckhang12@gmail.com>
2024-05-25 15:49:46 +0700

    Add: testing.vue

---
+++ pages/testing.vue
@@ -0,0 +1,101 @@
+<template>
+    <div>
+        <h1>Hello, h1 heading</h1>
+
+        <button @click="toggleBook">Toggle book</button>
+
+        
+        <div v-for="bok in books" v-if="showBook">
+            <book :bok="bok" :update-book="updateBook"></book>
+        </div>
+
+        <hr>
+
+        {{ books }}
+
+    </div>
+</template>
+
+<!-- <script>
+export default {
+    setup() {
+        function updateBook(book) {
+            book.isLiked = !book.isLiked
+            console.log("Update book")
+        }
+
+        function toggleBook() {
+            showBook.value = !showBook.value
+            console.log(showBook)
+        }
+
+
+        let books = reactive([
+            {
+                title: "The lord of the rings",
+                author: "Me",
+                isLiked: true
+            },
+            {
+                title: "Book 2",
+                author: "Me",
+                isLiked: false
+            },
+            {
+                title: "1984",
+                author: "Me",
+                isLiked: false
+            }
+        ])
+
+        let showBook = ref(true)
+
+        return { books, showBook, toggleBook, updateBook }
+    }
+}
+</script> -->
+
+<script setup>
+let {name} = defineProps(['name'])
+
+function updateBook(book) {
+    book.isLiked = !book.isLiked
+    console.log("Update book")
+}
+
+function toggleBook() {
+    showBook.value = !showBook.value
+    console.log(showBook)
+}
+
+let books = reactive([
+    {
+        title: "The lord of the rings",
+        author: "Me",
+        isLiked: true
+    },
+    {
+        title: "Book 2",
+        author: "Me",
+        isLiked: false
+    },
+    {
+        title: "1984",
+        author: "Me",
+        isLiked: false
+    }
+])
+
+// const state = reactive({
+//   showBook: true
+// })
+
+
+let showBook = ref(true)
+
+// return { books, showBook , toggleBook, updateBook}
+</script>
+
+<style scoped>
+
+</style>
