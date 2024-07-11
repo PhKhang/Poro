f990a9114203b91850eb5f38e040b19cefdd2bd4
PhKhang <trannguyenphuckhang12@gmail.com>
2024-07-11 17:28:52 +0700

    Update: before switching branch

--- src/server/api/auth/[...].ts
+++ src/server/api/auth/[...].ts
@@ -1,7 +1,24 @@
 import { NuxtAuthHandler } from '#auth'
 import CredentialsProvider from 'next-auth/providers/credentials'
+import GoogleProvider from 'next-auth/providers/google'
 import { UserModel } from '~/server/models/user';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import { PrismaClient } from '@prisma/client';
 import bcrypt from 'bcrypt'
+
+const prisma = new PrismaClient()
+
+async function getMe(session: any) {
+  return await $fetch('/api/me', {
+    method: 'POST',
+    query: {
+      API_ROUTE_SECRET: useRuntimeConfig().API_ROUTE_SECRET,
+    },
+    body: {
+      email: session?.user?.email,
+    },
+  })
+}
 
 export default NuxtAuthHandler({
   pages: {
@@ -9,6 +26,8 @@ export default NuxtAuthHandler({
   },
 
   secret: useRuntimeConfig().authSecret,
+  
+  // adapter: PrismaAdapter(prisma),
 
   providers: [
     // @ts-expect-error You need to use .default here for it to work during SSR. May be fixed via Vite at some point
@@ -16,6 +35,11 @@ export default NuxtAuthHandler({
     //    clientId: 'enter-your-client-id-here',
     //    clientSecret: 'enter-your-client-secret-here'
     // })
+    GoogleProvider.default({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET
+    }),
+    // @ts-expect-error You need to use .default here for it to work during SSR. May be fixed via Vite at some point
     CredentialsProvider.default({
       name: 'credentials',
       credentials: {},
@@ -64,6 +88,13 @@ export default NuxtAuthHandler({
 
       return session;
     }
+    
+    // session: async ({ session, token }) => {
+    //   const me = await getMe(session)
+
+    //   ;(session as any).subscribed = me?.subscribed
+    //   return Promise.resolve(session)
+    // }
   },
 
 })
