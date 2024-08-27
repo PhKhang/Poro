c44c19f3fe0474f0a9afcfac53b66ba58bb3853c
PhKhang <trannguyenphuckhang12@gmail.com>
2024-08-27 22:58:14 +0700

    Update: add username to leaderboard

--- src/server/controllers/dashboardController.ts
+++ src/server/controllers/dashboardController.ts
@@ -4,10 +4,22 @@ import { h } from "vue";
 
 const pipeline: PipelineStage[] = [
   {
+    $lookup: {
+      from: "User", // collection name in db
+      localField: "accountID",
+      foreignField: "id",
+      as: "username",
+    },
+  },
+  {
+    $unwind: "$username"
+  },
+  {
     $group: {
       _id: '$accountID',
       totalWorkingTime: { $sum: '$totalTime' },
-      createdAt: { $first: '$createdAt' }
+      createdAt: { $first: '$createdAt' },
+      name: { $first: '$username.name' }
     }
   },
   { $sort: { totalWorkingTime: -1 } }
@@ -17,50 +29,78 @@ let allRankings: ([] | null) = null
 
 export default {
 
+  async getJoin() {
+    const join = await SessionModel.aggregate([
+      {
+        $lookup: {
+          from: "User", // collection name in db
+          localField: "accountID",
+          foreignField: "id",
+          as: "username",
+        },
+      },
+      {
+        $unwind: "$username"
+      },
+      {
+        $project: {
+          "username.password": 0,
+          "username._id": 0,
+          "username.id": 0,
+          "username.email": 0,
+          "username.image": 0,
+          "username.__v": 0,
+          "username.createdAt:": 0,
+          "username.updatedAt:": 0,
+        }
+      },
+    ])
+    return join
+  },
   async getLeaderBoard(req: any) {
     allRankings = await SessionModel.aggregate(pipeline) as ([] | null)
     return allRankings
   },
   async getByTime(accountID: any) {
     const date = new Date()
-      const result = await SessionModel.aggregate([
-        {
-          $match: {
-            accountID: accountID,
-            totalTime: { $gt: 0 },
-            createdAt: { $gte: new Date(date.getFullYear(), date.getMonth(), 1) }
-          }
-        },
-      ])
-      
-      let totalTimeOfToday = 0
-      for (let i = 0; i < result.length; i++) {
-        if (result[i].createdAt.getDate() === date.getDate()) {
-          totalTimeOfToday += result[i].totalTime
+    const result = await SessionModel.aggregate([
+      {
+        $match: {
+          accountID: accountID,
+          totalTime: { $gt: 0 },
+          createdAt: { $gte: new Date(date.getFullYear(), date.getMonth(), 1) }
         }
+      },
+    ])
+
+    let totalTimeOfToday = 0
+    for (let i = 0; i < result.length; i++) {
+      if (result[i].createdAt.getDate() === date.getDate()) {
+        totalTimeOfToday += result[i].totalTime
       }
-      
-      let totalTimeOfWeek = 0
-      let today = new Date()
-      today.setDate(today.getDate() - today.getDay() + 1)
-      for (let i = 0; i < result.length; i++) {
-        if (result[i].createdAt.getDate() >= today.getDate()) {
-          totalTimeOfWeek += result[i].totalTime
-        }
+    }
+
+    let totalTimeOfWeek = 0
+    let today = new Date()
+    today.setDate(today.getDate() - today.getDay() + 1)
+    for (let i = 0; i < result.length; i++) {
+      if (result[i].createdAt.getDate() >= today.getDate()) {
+        totalTimeOfWeek += result[i].totalTime
       }
-      
-      const totalTimeOfMonth = result.reduce((total, obj) => total + obj.totalTime, 0)
-      
-      console.log('Calc:', totalTimeOfToday)
-      console.log('Calc:', totalTimeOfWeek)
-      console.log('Calc:', totalTimeOfMonth)
-      
-      console.log('getByTime', result)
-      return {
-        totalTimeOfToday,
-        totalTimeOfWeek,
-        totalTimeOfMonth
-      };
+    }
+
+    const totalTimeOfMonth = result.reduce((total, obj) => total + obj.totalTime, 0)
+
+    console.log('Calc:', totalTimeOfToday)
+    console.log('Calc:', totalTimeOfWeek)
+    console.log('Calc:', totalTimeOfMonth)
+
+    console.log('getByTime', result)
+    return {
+      totalTimeOfToday,
+      totalTimeOfWeek,
+      totalTimeOfMonth
+    };
   },
 
   async getCurrentStanding(accountID: any) {
